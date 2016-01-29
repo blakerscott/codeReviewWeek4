0425762b3995fbdf3300a78b046f72631fca8c54
Blake Scott <blakerscott@gmail.com>
2016-01-29 14:28:16 -0800

    add JQuery for submit button and checkboxes

--- js/scripts.js
+++ js/scripts.js
@@ -4,24 +4,41 @@ function Pizza(sizePizza, topping) {
 
 }
 
-
 Pizza.prototype.price = function() {
-  var startingPrice = 10;
-  for (var i = 0; i < this.topping; i++) {
-    startingPrice += 1;
-  }
-
-  if (this.sizePizza === "Small") {
-    startingPrice += 3;
-  }
-
-  if (this.sizePizza === "Medium") {
-    startingPrice += 5;
-  }
+  var Price = 10;
 
   if (this.sizePizza === "Large") {
-    startingPrice += 7;
+    Price = Price + 7 + (this.topping.length * 1);
   }
 
-  return startingPrice;
+  else if (this.sizePizza === "Medium") {
+    Price = Price  + 5 + (this.topping.length * 1);
+  }
+
+  else if (this.sizePizza === "Small") {
+    Price = Price  + 3 + (this.topping.length * 1);
+  }
+
+  else {
+    Price = Price + (this.topping.length * 1);
+  }
+
+  return Price;
 };
+
+
+$(document).ready(function() {
+  $("form#pizzaSelect").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSize = $("select#size").val();
+    var inputtedTopping = [];
+    $("input:checked").each(function() {
+      inputtedTopping.push($(this).val());
+    });
+    var newPizza = new Pizza(inputtedSize, inputtedTopping);
+    console.log("Pizza Object: ", newPizza);
+    $("ul#pizzaCost").append("<p>$" + newPizza.price() +".00</p>");
+
+    });
+  });
