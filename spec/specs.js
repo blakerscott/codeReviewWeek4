57263a49baac72d52ac701f1c93cbb07d7d92157
Blake Scott <blakerscott@gmail.com>
2016-01-29 09:12:16 -0800

    create first spec for pizza size

---
+++ spec/specs.js
@@ -0,0 +1,6 @@
+describe ("Pizza", function() {
+  it("will generate a Pizza based on input", function() {
+    var testPizza = new Pizza(true);
+    expect(testTicket.small).to.equal(true);
+  });
+});
