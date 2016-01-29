4f1cdeebfaf2fef4626221f8cfab7e5afaeda2bb
Blake Scott <blakerscott@gmail.com>
2016-01-29 09:29:11 -0800

    create second spec to test if number of toppings will modify price

--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,9 @@ describe ("Pizza", function() {
     var testPizza = new Pizza(true);
     expect(testPizza.sizePizza).to.equal(true);
   });
+
+  it("will modify price based on number of toppings selected", function(){
+    var testPizza = new Pizza(true);
+    expect(testPizza.topping).to.equal(7);
+  });
 });
